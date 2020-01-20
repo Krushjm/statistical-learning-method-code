3b6565c504b3c094bb1ea8baddfb6a9674a90a22
krushjm <krushjm@outlook.com>
2020-01-20 14:42:42 +0800

    2020-1-20 Improve usability of classify, add bayes estimation method.

--- NaiveBayesian/gaussian_naive_bayesian.py
+++ NaiveBayesian/gaussian_naive_bayesian.py
@@ -6,32 +6,50 @@ from sklearn.datasets import load_iris
 
 
 class GaussianBayesianClassify:
-    
+
     def __init__(self):
         pass
-    
+
+    def _maximum_likelihood_estimation(self, X, y):
+        p_y, p_x_y = dict(), dict()
+        for val_y in np.unique(y):
+            p_y[val_y] = np.sum(y == val_y) / y.shape[0]
+            for col in range(0, X.shape[1]):
+                p_x_y.update({
+                    (val_x, val_y): (
+                    np.sum(X[:, col] == val_x) * p_y[val_y] / np.sum(y == val_y)
+                    ) for val_x in np.unique(X[:, col])})
+        return p_y, p_x_y
+
+    def _bayes_estimation(self, X, y):
+        p_y, p_x_y = dict(), dict()
+        K = np.sum(np.unique(y))
+        for val_y in np.unique(y):
+            p_y[val_y] = (np.sum(y == val_y) + 1) / (y.shape[0] + K)
+            for col in range(0, X.shape[1]):
+                S_j = np.sum(np.unique(X[:, col]))
+                p_x_y.update({
+                    (val_x, val_y): (
+                    (np.sum(X[:, col] == val_x) * p_y[val_y] + 1) / (np.sum(y == val_y) + S_j)
+                    ) for val_x in np.unique(X[:, col])})
+        return p_y, p_x_y
+
     def fit(self, X, y, x):
-        if x[0] not in X:
-            print('x is not in dataset!')
-            return
-        p_y = dict()
-        p_x_y = dict()
-        for val in np.unique(y):
-            p_y[val] = np.sum(y == val) / y.shape[0]
-            # print(val, p_y[val])
-            for val2 in np.unique(X[:,0]):
-                p_x_y[(val2, val)] = np.sum(X[:,0] == val2) * p_y[val] / np.sum(y == val)
-                # print('col1', (val2, val), p_x_y[(val2, val)])
-            for val3 in np.unique(X[:,1]):
-                p_x_y[(val3, val)] = np.sum(X[:,1] == val3) * p_y[val] / np.sum(y == val)
-                # print('col2', (val3, val), p_x_y[(val3, val)])
-        result = {val: p_y[val] * p_x_y[(x[0], val)] * p_x_y[(x[1], val)] for val in p_y.keys()}
-        prob, y1 = max(zip(result.values(), result.keys()))
-        print((y1, float('%.5f' % prob)))
-    
+        if x not in X:
+            return print('x is not in dataset!')
+        prior_prob, conditional_prob = self._bayes_estimation(X, y)
+        posterior_prob = {}
+        for val_y in prior_prob.keys():
+            product = prior_prob[val_y]
+            for val_x in x:
+                product *= conditional_prob[(val_x, val_y)]
+            posterior_prob[val_y] = product
+        prob, class_ = max(zip(posterior_prob.values(), posterior_prob.keys()))
+        print((class_, float('%.5f' % prob)))
+
     def score(self):
         pass
-    
+
 
 if __name__ == "__main__":
     iris = load_iris()
@@ -41,4 +59,4 @@ if __name__ == "__main__":
     data = np.array(df.iloc[:100, [0, 1, -1]])
     X, y = data[:,:-1], data[:,-1]
     classify = GaussianBayesianClassify()
-    classify.fit(X, y, np.array([5.2, 3.1]))
+    classify.fit(X, y, np.array([5.9, 2.7]))
